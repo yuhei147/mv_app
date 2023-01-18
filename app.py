e0877b2252618314e6b2c5b83b1eec4d9e6d8def
yuhei147 <fallere140@gmail.com>
2023-01-18 22:38:45 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title('動画置き場')
+
+option = st.sidebar.selectbox(
+    '動画選択',
+    ['2022/01/01', '2022/01/02']
+)
+if option == '2022/01/01':
+    st.write('2022/01/01')
+    video_file = open('0101.mp4', 'rb')
+    video_bytes = video_file.read()
+    st.video(video_bytes)
+else:
+    st.write('2022/01/01')
+    video_file = open('0102.mp4', 'rb')
+    video_bytes = video_file.read()
+    st.video(video_bytes)
