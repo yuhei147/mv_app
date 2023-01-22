b94b52ac783f06c4bb780e5af0f87b23fd07a7e5
yuhei147 <fallere140@gmail.com>
2023-01-22 17:38:08 +0900

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title('動画置き場')
+st.write('右のセレクトボックスで表示したい動画を選択してください')
 
 option = st.sidebar.selectbox(
     '動画選択',
@@ -12,7 +13,7 @@ if option == '2022/01/01':
     video_bytes = video_file.read()
     st.video(video_bytes)
 else:
-    st.write('2022/01/01')
+    st.write('2022/01/02')
     video_file = open('0102.mp4', 'rb')
     video_bytes = video_file.read()
     st.video(video_bytes)
